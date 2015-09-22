d64d8bfab494dfbf755c618469fc2565b333d8fc
devkook <devinkook@gmail.com>
2015-09-23 08:26:02 +0900

    optparse를 이용한 인수해석 및 버전찍기

--- lib/how_old_au/command/options.rb
+++ lib/how_old_au/command/options.rb
@@ -6,7 +6,15 @@ module HowOldAu
   class Command
     module Options
       def self.parse!(argv)
-        {}
+        command_parser = OptionParser.new do |opt|
+          opt.on_head('-v','--version','Show program version') do |v|
+            opt.version = HowOldAu::VERSION
+            puts opt.version
+            exit
+          end
+        end
+
+        command_parser.parse!(argv)
       end
     end
   end
